14304d628877e733b4d71c5ce38c92aa9095b2f5
1Dragoon <D>
2023-03-15 00:09:43 -0700

    second try

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use std::{cmp::Ordering, collections::BTreeSet, fmt::Display};
 use num_traits::{Num, SaturatingAdd, SaturatingSub};
+use std::{cmp::Ordering, collections::BTreeSet, fmt::Display};
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub enum Unit<T> {
@@ -81,7 +81,7 @@ impl<T: Num + SaturatingSub + SaturatingAdd + Ord + Eq + Copy + Display> Unit<T>
     }
 }
 
-impl<T: Ord> PartialOrd for Unit<T> {
+impl<T: Num + Ord + SaturatingSub> PartialOrd for Unit<T> {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         match self {
             Unit::Single(s) => match other {
@@ -98,16 +98,18 @@ impl<T: Ord> PartialOrd for Unit<T> {
             },
             Unit::Range((sl, sh)) => match other {
                 Unit::Single(o) => {
-                    if o < sl {
-                        Some(Ordering::Less)
-                    } else if o > sh {
+                    if sl > o {
                         Some(Ordering::Greater)
+                    } else if sh < o {
+                        Some(Ordering::Less)
                     } else {
                         Some(Ordering::Equal)
                     }
                 }
                 Unit::Range((ol, oh)) => {
-                    if sl == ol && sh == oh {
+                    // Overlapping or directly adjacent is also equal
+                    if *sl <= *oh && ol <= sh || (*sl).saturating_sub(&T::one()) == *oh || (*sh).saturating_sub(&T::one()) == *ol {
+                    // if sl == ol && sh == oh {
                         Some(Ordering::Equal)
                     } else if sl > oh {
                         Some(Ordering::Greater)
@@ -121,7 +123,7 @@ impl<T: Ord> PartialOrd for Unit<T> {
     }
 }
 
-impl<T: Ord> Ord for Unit<T> {
+impl<T: Num + Ord + SaturatingSub> Ord for Unit<T> {
     fn cmp(&self, other: &Self) -> Ordering {
         self.partial_cmp(other).unwrap()
     }
@@ -134,10 +136,13 @@ struct Ranger<T> {
 
 impl<T: Display> Display for Ranger<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for u in self.set.iter().take(self.set.len()-1) {
+        for u in self.set.iter().take(self.set.len().saturating_sub(1)) {
             write!(f, "{},", u)?
         }
-        write!(f, "{}", self.set.iter().last().unwrap())
+        if let Some(last) = self.set.iter().last() {
+            write!(f, "{}", last)?
+        }
+        Ok(())
     }
 }
 
@@ -163,6 +168,122 @@ impl<T: Num + SaturatingSub + SaturatingAdd + Ord + Eq + Copy + std::fmt::Displa
         }
         self.set.insert(value);
     }
+
+    fn puts(&mut self, v: T) {
+        // println!("put {v}");
+        let value = Unit::Single(v);
+        let one_less = Unit::Single(v.saturating_sub(&T::one()));
+        let one_more = Unit::Single(v.saturating_add(&T::one()));
+        let mut added = false;
+        if let Some(r) = self.set.get(&value) {
+            added = true;
+            match *r {
+                Unit::Single(_) => (), // Single exactly matched single; do nothing
+                Unit::Range((sl, sh)) => {
+                    if let Some(Unit::Range((ol, oh))) = self.set.get(&Unit::Single(sl)).or(self.set.get(&Unit::Single(sh))) {
+                        self.set.replace(Unit::Range((sl.min(*ol), sh.max(*oh))));
+                    }
+                    if let Some(Unit::Range((ol, oh))) = self.set.get(&Unit::Single(sl)).or(self.set.get(&Unit::Single(sh))) {
+                        self.set.replace(Unit::Range((sl.min(*ol), sh.max(*oh))));
+                    }
+                    // if let Some(Unit::Range((ol, oh))) = self.set.replace(Unit::Single(sl)).or(self.set.replace(Unit::Single(sh))) {
+                    //     println!("double replaced!");
+                    //     self.set.replace(Unit::Range((sl.min(ol), sh.max(oh))));
+                    // }
+                },
+            }
+        }
+        if let Some(r) = self.set.get(&one_less) {
+            // println!("added {v}");
+            added = true;
+            match *r {
+                Unit::Single(less) => {
+                    if let Some(Unit::Range((ol, oh))) = self.set.replace(Unit::Range((less, v))) {
+                        self.set.replace(Unit::Range((ol.min(less), oh.max(v))));
+                    }
+                },
+                Unit::Range((sl, sh)) => {
+                    if let Some(Unit::Range((ol, oh))) = self.set.replace(Unit::Range((sl, sh))) {
+                        self.set.replace(Unit::Range((sl.min(ol), sh.max(oh).max(v))));
+                    }
+                },
+            }
+            // self.set.replace(Unit::Range(((*l).min(v), (*h).max(v))));
+        }
+        if let Some(r) = self.set.get(&one_more) {
+            added = true;
+            match *r {
+                Unit::Single(more) => {
+                    if let Some(Unit::Range((ol, oh))) = self.set.replace(Unit::Range((v, more))) {
+                        self.set.replace(Unit::Range((ol.min(v), oh.max(more))));
+                    }
+                },
+                Unit::Range((sl, sh)) => {
+                    if let Some(Unit::Range((ol, oh))) = self.set.replace(Unit::Range((sl, sh))) {
+                        self.set.replace(Unit::Range((sl.min(ol).min(v), sh.max(oh))));
+                    }
+                },
+            }
+        }
+        if !added {
+            self.set.insert(value);
+        }
+        // if self.set.contains(&ol) {
+        //     if let Some(r) = self.set.replace(ol) {
+        //         match r {
+        //             Unit::Single(s) => {
+        //                 if s != v {
+        //                     if let Some(rep) = self.set.replace(Unit::Range((s.min(v), v.max(v)))) {
+        //                         // println!("replaced ol {r} with {}", rep);
+        //                     } else {
+        //                         // println!("pushed ol {r}");
+        //                     }
+        //                 }
+        //             },
+        //             Unit::Range((l, h)) => {
+        //                 if let Some(rep) = self.set.replace(Unit::Range((l.min(v), h.max(v)))) {
+        //                     // println!("replaced ol {r} with {}", rep);
+        //                 } else {
+        //                     // println!("pushed ol {r}");
+        //                 }
+        //             },
+        //         }
+        //     }
+        //     // println!("added ol {ol}: {self}");
+        //     return
+        // }
+        // if self.set.contains(&om) {
+        //     if let Some(r) = self.set.replace(om) {
+        //         match r {
+        //             Unit::Single(s) => {
+        //                 if s != v {
+        //                     if let Some(rep) = self.set.replace(Unit::Range((s.min(v), s.max(v)))) {
+        //                         // println!("replaced om {r} with {}", rep);
+        //                     } else {
+        //                         // println!("pushed om {r}");
+        //                     }
+        //                 }
+        //             },
+        //             Unit::Range((l, h)) => {
+        //                 if let Some(rep) = self.set.replace(Unit::Range((l.min(v), h.max(v)))) {
+        //                     // println!("replaced om {r} with {}", rep);
+        //                 } else {
+        //                     // println!("pushed om {r}");
+        //                 }
+        //             }
+        //         }
+        //     }
+        //     // println!("added om {om}: {self}");
+        //     return
+        // }
+        // let c = self.set.contains(&value);
+        // if !c {
+        //     self.set.insert(value);
+        //     // println!("added {v}: {}", self);
+        // } else {
+        //     // println!("already has {v}: {self}");
+        // }
+    }
 }
 
 pub fn add(left: usize, right: usize) -> usize {
@@ -178,16 +299,45 @@ mod tests {
     #[test]
     fn it_works() {
         let input_numbers: &[u16] = &[
-            0, 1, 2, 4, 6, 7, 8, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28, 29,
-            30, 31, 32, 33, 35, 36, 37, 38, 39,
+            0, 1, 2, 4, 6, 7, 8, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28,
+            29, 30, 31, 32, 33, 35, 36, 37, 38, 39,
         ];
-        let mut ranger = Ranger{
+        let mut ranger = Ranger {
             set: BTreeSet::new(),
         };
         for num in input_numbers {
             ranger.put(*num);
         }
-        println!("{}", ranger)
+        println!("{}", ranger);
+        drop(ranger);
+        let mut ranger = Ranger {
+            set: BTreeSet::new(),
+        };
+        for num in input_numbers {
+            ranger.puts(*num);
+        }
+        println!("{}", ranger);
+        drop(ranger);
+        let input_numbers: &[u16] = &[
+            33, 35, 23, 20, 28, 19, 18, 14, 25, 21, 38, 6, 39, 27, 11, 17, 7, 12, 31, 15, 32, 4,
+            29, 36, 22, 1, 0, 37, 30, 8, 24, 16, 2,
+        ];
+        let mut ranger = Ranger {
+            set: BTreeSet::new(),
+        };
+        for num in input_numbers {
+            ranger.put(*num);
+        }
+        println!("{}", ranger);
+        drop(ranger);
+        let mut ranger = Ranger {
+            set: BTreeSet::new(),
+        };
+        for num in input_numbers {
+            ranger.puts(*num);
+        }
+        println!("{}", ranger);
+        drop(ranger);
     }
 }
 
