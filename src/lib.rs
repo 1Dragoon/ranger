7573ce02c6f37903db6fa8c719888444e9f81aa0
1Dragoon <D>
2023-03-18 01:31:36 -0700

    now its really messing up

--- src/lib.rs
+++ src/lib.rs
@@ -4,57 +4,63 @@ use std::{cmp::Ordering, collections::BTreeSet, fmt::Display};
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub enum Unit<T: Num + SaturatingAdd> {
     Single(T),
-    Range((T, T)), // lower bound and upper bound
+    Range((T, T)),
 }
 
-impl<T: Num + Ord + SaturatingAdd + Copy + Display> Unit<T> {
-    fn umm(&self, other: &Self) -> Option<Unit<T>> {
-        if self == other {
-            return None;
+impl<T: Num + Ord + SaturatingAdd + Copy> Unit<T> {
+    fn merged(&self, other: &Self) -> Option<Unit<T>> {
+        let l = self.low();
+        let h = other.high();
+        if l < h && l.saturating_add(&T::one()) == h {
+            return Some(Unit::Range((l, h)))
         }
-        let left = self.min(other);
-        let right = self.max(other);
-        match left {
-            Unit::Single(l) => match right {
-                Unit::Single(r) => {
-                    if l < r && l.saturating_add(&T::one()) == *r {
-                        Some(Unit::Range((*l, *r)))
+        match self {
+            Unit::Single(s) => match other {
+                Unit::Single(o) => {
+                    if s < o && s.saturating_add(&T::one()) == *o {
+                        Some(Unit::Range((*s, *o)))
                     } else {
                         None
                     }
                 }
-                Unit::Range((rl, r)) => {
-                    if l < rl {
-                        if l.saturating_add(&T::one()) == *rl {
-                            Some(Unit::Range((*l, *r)))
-                        } else {
-                            None
-                        }
+                Unit::Range((ol, oh)) => {
+                    if s < ol && s.saturating_add(&T::one()) == *ol {
+                        Some(Unit::Range((*s, *oh)))
                     } else {
                         None
                     }
                 }
             },
-            Unit::Range((ll, lr)) => match other {
-                Unit::Single(r) => {
-                    if lr < r {
-                        if lr.saturating_add(&T::one()) == *r {
-                            Some(Unit::Range((*ll, *r)))
-                        } else {
-                            None
-                        }
+            Unit::Range((sl, sh)) => match other {
+                Unit::Single(o) => {
+                    if sh < o && sh.saturating_add(&T::one()) == *o {
+                        Some(Unit::Range((*sl, *o)))
                     } else {
                         None
                     }
                 }
-                Unit::Range((rl, rr)) => {
-                    if lr < rl && lr.saturating_add(&T::one()) == *rl || ll <= rr && rl <= lr {
-                        Some(Unit::Range((*ll, *rr)))
+                Unit::Range((ol, oh)) => {
+                    if sh < ol && sh.saturating_add(&T::one()) == *ol || sl <= oh && ol <= sh {
+                        Some(Unit::Range((*sl, *oh)))
                     } else {
                         None
                     }
                 }
             },
+        }
+    }
+
+    fn low(&self) -> T {
+        match self {
+            Unit::Single(l) => *l,
+            Unit::Range((l, _)) => *l,
+        }
+    }
+
+    fn high(&self) -> T {
+        match self {
+            Unit::Single(h) => *h,
+            Unit::Range((_, h)) => *h,
         }
     }
 }
@@ -68,14 +74,19 @@ impl<T: Num + SaturatingAdd + Display> Display for Unit<T> {
     }
 }
 
-impl<T: Num + SaturatingAdd + Ord> Ord for Unit<T> {
+impl<T: Num + SaturatingAdd + Ord + Copy> Ord for Unit<T> {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.partial_cmp(other).unwrap_or(Ordering::Less)
+        self.partial_cmp(other).unwrap()
     }
 }
 
-impl<T: Num + SaturatingAdd + Ord> PartialOrd for Unit<T> {
+impl<T: Num + SaturatingAdd + Ord + Copy> PartialOrd for Unit<T> {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        // let l = self.low();
+        // let h = other.high();
+        // if l < h && l.saturating_add(&T::one()) == h {
+        //     return                         Some(Ordering::Equal)
+        // }
         match self {
             Unit::Single(s) => match other {
                 Unit::Single(o) => Some(s.cmp(o)),
@@ -85,7 +96,7 @@ impl<T: Num + SaturatingAdd + Ord> PartialOrd for Unit<T> {
                     } else if s > oh {
                         Some(Ordering::Greater)
                     } else {
-                        None
+                        Some(Ordering::Equal)
                     }
                 }
             },
@@ -96,18 +107,16 @@ impl<T: Num + SaturatingAdd + Ord> PartialOrd for Unit<T> {
                     } else if sh < o {
                         Some(Ordering::Less)
                     } else {
-                        None
+                        Some(Ordering::Equal)
                     }
                 }
                 Unit::Range((ol, oh)) => {
-                    if sl == ol && sh == oh {
-                        Some(Ordering::Equal)
-                    } else if sl > oh {
+                    if sl > oh {
                         Some(Ordering::Greater)
                     } else if sh < ol {
                         Some(Ordering::Less)
                     } else {
-                        None
+                        Some(Ordering::Equal)
                     }
                 }
             },
@@ -115,17 +124,14 @@ impl<T: Num + SaturatingAdd + Ord> PartialOrd for Unit<T> {
     }
 }
 
-#[derive(Debug)]
-struct Ranger<T: Num + SaturatingAdd> {
-    set: BTreeSet<Unit<T>>,
-}
+struct Ranger<T: Num + SaturatingAdd>(BTreeSet<Unit<T>>);
 
 impl<T: Num + SaturatingAdd + Display> Display for Ranger<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for u in self.set.iter().take(self.set.len().saturating_sub(1)) {
+        for u in self.0.iter().take(self.0.len().saturating_sub(1)) {
             write!(f, "{},", u)?
         }
-        if let Some(last) = self.set.iter().last() {
+        if let Some(last) = self.0.iter().last() {
             write!(f, "{}", last)?
         }
         Ok(())
@@ -133,22 +139,25 @@ impl<T: Num + SaturatingAdd + Display> Display for Ranger<T> {
 }
 
 impl<T: Num + SaturatingAdd + Ord + Copy + Display> Ranger<T> {
-    fn put(&mut self, v: T) {
+    pub fn new() -> Self {
+        Self(BTreeSet::new())
+    }
+    pub fn insert(&mut self, v: T) {
         let value = Unit::Single(v);
-        self.set.insert(value);
-        let mut right_side = self.set.split_off(&value);
-        if let Some(left) = self.set.last().copied() {
-            if let Some(merged) = left.umm(&value) {
-                    right_side.remove(&value);
-                    self.set.remove(&left);
-                    right_side.insert(merged);
-                }
+        self.0.insert(value);
+        let mut right_side = self.0.split_off(&value);
+        if let Some(left) = self.0.last().copied() {
+            if let Some(merged) = left.merged(&value) {
+                right_side.remove(&value);
+                self.0.remove(&left);
+                right_side.insert(merged);
             }
+        }
 
-        while let Some(new_right) = right_side.iter().nth(1).copied() {
+        while let Some(right) = right_side.iter().nth(1).copied() {
             let new_val = right_side.first().copied().unwrap();
-            if let Some(m) = new_val.umm(&new_right) {
-                right_side.remove(&new_right);
+            if let Some(m) = new_val.merged(&right) {
+                right_side.remove(&right);
                 right_side.remove(&new_val);
                 right_side.insert(m);
             } else {
@@ -156,76 +165,54 @@ impl<T: Num + SaturatingAdd + Ord + Copy + Display> Ranger<T> {
             }
         }
 
-        self.set.extend(right_side);
+        self.0.extend(right_side);
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::collections::BTreeSet;
-
-    use rand::{seq::SliceRandom, thread_rng};
-
     use super::*;
+    use rand::{seq::SliceRandom, thread_rng};
 
     #[test]
     fn it_works() {
-        // Expected: 0-2,4,6-8,11-12,14-25,27-33,35-39
-        let input_numbers: &[u16] = &[
+        let input_numbers: &[u8] = &[
             0, 1, 2, 4, 6, 7, 8, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28,
             29, 30, 31, 32, 33, 35, 36, 37, 38, 39,
         ];
-        let mut ranger = Ranger {
-            set: BTreeSet::new(),
-        };
+        let mut ranger = Ranger::new();
         for num in input_numbers {
-            ranger.put(*num);
+            ranger.insert(*num);
         }
-        println!("{}", ranger);
-        assert!(ranger.to_string() == "0-2,4,6-8,11-12,14-25,27-33,35-39");
+        assert_eq!(ranger.to_string(), "0-2,4,6-8,11-12,14-25,27-33,35-39");
         drop(ranger);
-        println!(
-            "
-        
-        Tada!
-
-        "
-        );
-        let input_numbers: &[u16] = &[
-            33, 35, 23, 20, 28, 19, 18, 14, 25, 21, 38, 6, 39, 27, 11, 17, 7, 12, 31, 15, 32, 4,
-            29, 36, 22, 1, 0, 37, 30, 8, 24, 16, 2, 65535, 65534, 65531, 65533, 65532, 65530
+        let input_numbers: &[u8] = &[
+            33, 35, 23, 255, 254, 20, 28, 19, 18, 14, 25, 21, 253, 38, 6, 39, 27, 11, 17, 7, 12,
+            128, 126, 31, 15, 32, 4, 29, 36, 22, 1, 0, 37, 30, 8, 24, 16, 2,
         ];
-        let mut ranger = Ranger {
-            set: BTreeSet::new(),
-        };
+        let mut ranger = Ranger::new();
         for num in input_numbers {
-            ranger.put(*num);
+            ranger.insert(*num);
         }
-        println!(
-            "
-        
-        
-        {}",
-            ranger
+        assert_eq!(
+            ranger.to_string(),
+            "0-2,4,6-8,11-12,14-25,27-33,35-39,126,128,253-255"
         );
-        assert!(ranger.to_string() == "0-2,4,6-8,11-12,14-25,27-33,35-39,65530-65535");
         drop(ranger);
 
-        for _ in 0..100 {
+        for _ in 0..100_000 {
             let mut myvec = input_numbers.to_vec();
             myvec.shuffle(&mut thread_rng());
-            // println!("{:?}", myvec);
-            let mut ranger = Ranger {
-                set: BTreeSet::new(),
-            };
             println!("{:?}", myvec);
+            let mut ranger = Ranger::new();
             for num in myvec {
-                ranger.put(num);
+                ranger.insert(num);
             }
-            assert!(ranger.to_string() == "0-2,4,6-8,11-12,14-25,27-33,35-39,65530-65535");
+
+            assert_eq!(
+                ranger.to_string(),
+                "0-2,4,6-8,11-12,14-25,27-33,35-39,126,128,253-255"
+            );
         }
     }
 }
-
-// 0-2,8,4,6-8,11,12,14-25,27-33,35-39
-// 0-2,6-8,11-12,14-25,27-33,35-39,4
