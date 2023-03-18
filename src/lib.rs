03a89c5e4e4a2047a94df99438e79e7c9dd52a5e
1Dragoon <D>
2023-03-18 14:48:16 -0700

    forgot signed

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,22 @@
 use itertools::Itertools;
-use num_traits::{bounds::UpperBounded, Num, PrimInt};
+use num_traits::{Num, SaturatingSub};
 use std::{cmp::Ordering, collections::BTreeSet, fmt::Display};
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub enum Unit<T: Num> {
+pub enum Unit<T: Num + SaturatingSub> {
     Singleton(T),
     Range((T, T)),
 }
 
-impl<T: Num + UpperBounded + Copy> Unit<T> {
+impl<T: Num + Ord + SaturatingSub + Copy> Unit<T> {
     fn merged(&self, other: &Self) -> Option<Unit<T>> {
-        if self.high() == &T::max_value() || *other.low() - *self.high() == T::one() {
+        if other.low().saturating_sub(self.high()).is_one() {
             Some(Unit::Range((*self.low(), *other.high())))
         } else {
             None
         }
     }
-}
 
-impl<T: Num> Unit<T> {
     fn low(&self) -> &T {
         match self {
             Unit::Singleton(l) => l,
@@ -34,13 +32,13 @@ impl<T: Num> Unit<T> {
     }
 }
 
-impl<T: Num + Ord> Ord for Unit<T> {
+impl<T: Num + SaturatingSub + Ord + Copy> Ord for Unit<T> {
     fn cmp(&self, other: &Self) -> Ordering {
         unsafe { self.partial_cmp(other).unwrap_unchecked() }
     }
 }
 
-impl<T: Num + Ord> PartialOrd for Unit<T> {
+impl<T: Num + SaturatingSub + Ord + Copy> PartialOrd for Unit<T> {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         if self.low() > other.high() {
             Some(Ordering::Greater)
@@ -52,7 +50,7 @@ impl<T: Num + Ord> PartialOrd for Unit<T> {
     }
 }
 
-impl<T: Num + Display> Display for Unit<T> {
+impl<T: Num + SaturatingSub + Display> Display for Unit<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Unit::Singleton(s) => write!(f, "{s}"),
@@ -61,9 +59,10 @@ impl<T: Num + Display> Display for Unit<T> {
     }
 }
 
-struct Ranger<T: PrimInt>(BTreeSet<Unit<T>>);
+#[derive(Default)]
+pub struct Ranger<T: Num + SaturatingSub>(BTreeSet<Unit<T>>);
 
-impl<T: PrimInt + Display> Display for Ranger<T> {
+impl<T: Num + SaturatingSub + Display> Display for Ranger<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for u in self.0.iter().take(self.0.len().saturating_sub(1)) {
             write!(f, "{},", u)?
@@ -75,7 +74,7 @@ impl<T: PrimInt + Display> Display for Ranger<T> {
     }
 }
 
-impl<T: PrimInt> Ranger<T> {
+impl<T: Num + SaturatingSub + Ord + Copy> Ranger<T> {
     pub fn new() -> Self {
         Self(BTreeSet::new())
     }
@@ -130,9 +129,9 @@ mod tests {
         }
         assert_eq!(ranger.to_string(), "0-2,4,6-8,11-12,14-25,27-33,35-39");
         drop(ranger);
-        let input_numbers: &[u8] = &[
-            33, 35, 23, 255, 254, 20, 28, 19, 18, 14, 25, 21, 253, 38, 6, 39, 27, 11, 17, 7, 12,
-            128, 126, 31, 15, 32, 4, 29, 36, 22, 1, 0, 37, 30, 8, 24, 16, 2,
+        let input_numbers: &[i8] = &[
+            -1, 33, 35, 23, 20, -128, 28, 19, 18, 14, 25, 21, 127, 38, 6, 39, 27, 11, 17, 7, 12,
+            126, -126, 31, 15, 32, 4, 29, 36, 22, 1, 0, 37, 30, 8, 24, 16, 2, -127, 125,
         ];
         let mut ranger = Ranger::new();
         for num in input_numbers {
@@ -140,11 +139,13 @@ mod tests {
         }
         assert_eq!(
             ranger.to_string(),
-            "0-2,4,6-8,11-12,14-25,27-33,35-39,126,128,253-255"
+            "-128--126,-1-2,4,6-8,11-12,14-25,27-33,35-39,125-127"
         );
+        println!("{:?} -> {ranger}", input_numbers);
         drop(ranger);
-        
-        for _ in 0..100_000 { // Just to be extra thorough
+
+        // Rule out edge cases
+        for _ in 0..10_000 {
             let mut myvec = input_numbers.to_vec();
             myvec.shuffle(&mut thread_rng());
             // println!("{:?}", myvec);
@@ -155,7 +156,7 @@ mod tests {
 
             assert_eq!(
                 ranger.to_string(),
-                "0-2,4,6-8,11-12,14-25,27-33,35-39,126,128,253-255"
+                "-128--126,-1-2,4,6-8,11-12,14-25,27-33,35-39,125-127"
             );
         }
     }
