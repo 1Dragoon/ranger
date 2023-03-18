bac78b38ea77195691657c3b0c0f1c885c9359ad
1Dragoon <D>
2023-03-18 04:11:55 -0700

    moar shrinkage

--- src/lib.rs
+++ src/lib.rs
@@ -1,78 +1,69 @@
-use num_traits::{Num, SaturatingAdd};
+use itertools::Itertools;
+use num_traits::PrimInt;
 use std::{cmp::Ordering, collections::BTreeSet, fmt::Display};
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub enum Unit<T: Num + SaturatingAdd> {
-    Single(T),
+pub enum Unit<T: PrimInt> {
+    Singleton(T),
     Range((T, T)),
 }
 
-impl<T: Num + Ord + SaturatingAdd + Copy> Unit<T> {
+impl<T: PrimInt + Copy> Unit<T> {
     fn merged(&self, other: &Self) -> Option<Unit<T>> {
-        let sh = self.high();
-        let ol = other.low();
-        let sl = self.low();
-        let oh = other.high();
-        if sl < oh && sl.saturating_add(&T::one()) == *oh
-            || sh < ol && sh.saturating_add(&T::one()) == *ol
-        {
-            Some(Unit::Range((*sl, *oh)))
+        if self.high() == &T::max_value() || *other.low() - *self.high() == T::one() {
+            Some(Unit::Range((*self.low(), *other.high())))
         } else {
             None
         }
     }
 }
 
-impl<T: Num + SaturatingAdd> Unit<T> {
+impl<T: PrimInt> Unit<T> {
     fn low(&self) -> &T {
         match self {
-            Unit::Single(l) => l,
+            Unit::Singleton(l) => l,
             Unit::Range((l, _)) => l,
         }
     }
 
     fn high(&self) -> &T {
         match self {
-            Unit::Single(h) => h,
+            Unit::Singleton(h) => h,
             Unit::Range((_, h)) => h,
         }
     }
 }
 
-impl<T: Num + SaturatingAdd + Display> Display for Unit<T> {
+impl<T: PrimInt + Display> Display for Unit<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Unit::Single(s) => write!(f, "{s}"),
+            Unit::Singleton(s) => write!(f, "{s}"),
             Unit::Range((t, u)) => write!(f, "{t}-{u}"),
         }
     }
 }
 
-impl<T: Num + SaturatingAdd + Ord> Ord for Unit<T> {
+impl<T: PrimInt> Ord for Unit<T> {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.partial_cmp(other).unwrap()
+        unsafe { self.partial_cmp(other).unwrap_unchecked() }
     }
 }
 
-impl<T: Num + SaturatingAdd + Ord> PartialOrd for Unit<T> {
+impl<T: PrimInt> PartialOrd for Unit<T> {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        let sh = self.high();
-        let ol = other.low();
-        let sl = self.low();
-        let oh = other.high();
-        if sl > oh {
+        if self.low() > other.high() {
             Some(Ordering::Greater)
-        } else if sh < ol {
+        } else if self.high() < other.low() {
             Some(Ordering::Less)
         } else {
-            Some(sh.cmp(oh))
+            Some(self.high().cmp(other.high()))
         }
     }
 }
 
-struct Ranger<T: Num + SaturatingAdd>(BTreeSet<Unit<T>>);
+struct Ranger<T: PrimInt>(BTreeSet<Unit<T>>);
 
-impl<T: Num + SaturatingAdd + Display> Display for Ranger<T> {
+impl<T: PrimInt + Display> Display for Ranger<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for u in self.0.iter().take(self.0.len().saturating_sub(1)) {
             write!(f, "{},", u)?
@@ -84,35 +75,33 @@ impl<T: Num + SaturatingAdd + Display> Display for Ranger<T> {
     }
 }
 
-impl<T: Num + SaturatingAdd + Ord + Copy + Display> Ranger<T> {
+impl<T: PrimInt + Copy + Display> Ranger<T> {
     pub fn new() -> Self {
         Self(BTreeSet::new())
     }
 
     pub fn insert(&mut self, v: T) {
-        let value = Unit::Single(v);
+        let value = Unit::Singleton(v);
         self.0.insert(value);
-        let mut right_side = self.0.split_off(&value);
-        if let Some(left) = self.0.last().copied() {
-            if let Some(merged) = left.merged(&value) {
-                right_side.remove(&value);
-                self.0.remove(&left);
-                right_side.insert(merged);
+        let mut high_side = self.0.split_off(&value);
+        if let Some(low) = self.0.last().copied() {
+            if let Some(merged) = low.merged(&value) {
+                high_side.remove(&value);
+                self.0.remove(&low);
+                high_side.insert(merged);
             }
         }
-
-        while let Some(right) = right_side.iter().nth(1).copied() {
-            let new_val = right_side.first().copied().unwrap();
-            if let Some(m) = new_val.merged(&right) {
-                right_side.remove(&right);
-                right_side.remove(&new_val);
-                right_side.insert(m);
-            } else {
-                break;
-            }
+        while let Some((m, l, h)) = high_side
+            .iter()
+            .copied()
+            .next_tuple()
+            .and_then(|(l, h)| l.merged(&h).map(|m| (m, l, h)))
+        {
+            high_side.remove(&l);
+            high_side.remove(&h);
+            high_side.insert(m);
         }
-
-        self.0.extend(right_side);
+        self.0.extend(high_side);
     }
 }
 
@@ -151,7 +140,7 @@ mod tests {
         for _ in 0..100_000 {
             let mut myvec = input_numbers.to_vec();
             myvec.shuffle(&mut thread_rng());
-            println!("{:?}", myvec);
+            // println!("{:?}", myvec);
             let mut ranger = Ranger::new();
             for num in myvec {
                 ranger.insert(num);
